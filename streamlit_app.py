135a00480d383a67bf09bbf3c2f49485337e7f04
cheesama <cheehoon12@hanmail.net>
2021-02-16 00:38:24 +0900

    recover inference spinner

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ import streamlit as st
 st.title('Prototyping NLP models with Pororo')
 
 # Load similarity model
+@st.cache(allow_output_mutation=True)
 def load_similarity_model():
     st.subheader('Semantic Textual Similarity')
     with st.spinner('Loading similarity model...'):
@@ -15,6 +16,7 @@ def load_similarity_model():
         return similarity_model
 
 # Load sentiment_analysis model
+@st.cache(allow_output_mutation=True)
 def load_sentiment_model():
     st.subheader('Sentiment Analysis')
     with st.spinner('Loading sentiment_analysis model...'):
@@ -23,6 +25,7 @@ def load_sentiment_model():
         return sentiment_model
 
 # Load named entity recognition model
+@st.cache(allow_output_mutation=True)
 def load_ner_model():
     st.subheader('Named Entity Recognition')
     with st.spinner('Loading sentiment_analysis model...'):
@@ -35,13 +38,15 @@ if __name__ == '__main__':
     sim_query1_input = st.text_input('query1:')
     sim_query2_input = st.text_input('query2:')
     if sim_query1_input != '' and sim_query2_input != '':
-        st.write(f'Similarity: {similarity_model(sim_query1_input, sim_query2_input)}')
+        with st.spinner('Predicting...'):
+            st.write(f'Similarity: {similarity_model(sim_query1_input, sim_query2_input)}')
 
     sentiment_model = load_sentiment_model()
     sentiment_query_input = st.text_input('query:')
     if sentiment_query_input != '':
-        st.write('Result:')
-        st.json(sentiment_model(sentiment_query_input, show_probs=True))
+        with st.spinner('Predicting...'):
+            st.write('Result:')
+            st.json(sentiment_model(sentiment_query_input, show_probs=True))
 
     
 
