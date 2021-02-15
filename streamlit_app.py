db8a84abde65038722243b74da8610d56c111083
cheesama <cheehoon12@hanmail.net>
2021-02-16 00:07:33 +0900

    modify model loading seq

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,34 +3,33 @@ from pororo import Pororo
 import streamlit as st
 
 # Set page title
-st.title('Prototyping NLP models with Pororo :robot:')
+st.title('Prototyping NLP models with Pororo')
 
 # Load similarity model
 st.subheader('Semantic Textual Similarity')
 with st.spinner('Loading similarity model...'):
     similarity_model = Pororo(task="similarity", lang="ko")
 
-    query1_input = st.text_input('query1:')
-    query2_input = st.text_input('query2:')
+sim_query1_input = st.text_input('query1:')
+sim_query2_input = st.text_input('query2:')
 
-    if query1_input != '' and query2_input != '':
-        with st.spinner('Calculating Similarity...'):
-             st.write(f'Similarity: {similarity_model(query1_input, query2_input)}')
-             
+if sim_query1_input != '' and sim_query2_input != '':
+    with st.spinner('Calculating Similarity...'):
+        st.write(f'Similarity: {similarity_model(sim_query1_input, sim_query2_input)}')
+
 # Load sentiment_analysis model
 st.subheader('Sentiment Analysis')
 with st.spinner('Loading sentiment_analysis model...'):
     sentiment_model = Pororo(task="sentiment", model="brainbert.base.ko.shopping", lang="ko")
 
-    query_input = st.text_input('query:')
+sentiment_query_input = st.text_input('query:')
 
-    if query_input != '':
-        with st.spinner('Predicting...'):
-            st.write('Result:')
-            st.json(sentiment_model(query_input, show_probs=True))
+if sentiment_query_input != '':
+    with st.spinner('Predicting...'):
+        st.write('Result:')
+        st.json(sentiment_model(sentiment_query_input, show_probs=True))
 
 # Load named entity recognition model
 st.subheader('Named Entity Recognition')
 with st.spinner('Loading sentiment_analysis model...'):
     ner_model = Pororo(task="ner", lang="ko")
-
