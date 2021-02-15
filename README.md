793513895edd7c19167912f86e09b1c780ccc8a3
cheesama <cheehoon12@hanmail.net>
2021-02-15 23:43:54 +0900

    fisrt commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rodi
 Rapid pOroro Demo Inferencer
+
+Demo Inferencer page for :penguin:(:robot_face: is a friend of :penguin:)
---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+from pororo import Pororo
+
+import streamlit as st
+
+# Set page title
+st.title('Prototyping NLP models with Pororo')
