c276efe0ec51e9ce2a4c3185be5600943fd2a23c
cheesama <cheehoon12@hanmail.net>
2021-02-15 23:48:49 +0900

    loading several models

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# rodi
+# rodi:robot:
 Rapid pOroro Demo Inferencer
 
 Demo Inferencer page for :penguin:(:robot: is a friend of :penguin:)
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,16 @@ import streamlit as st
 
 # Set page title
 st.title('Prototyping NLP models with Pororo')
+
+# Load similarity model
+with st.spinner('Loading similarity model...'):
+    similarity_model = Pororo(task="similarity", lang="ko")
+
+# Load sentiment_analysis model
+with st.spinner('Loading sentiment_analysis model...'):
+    sentiment_model = Pororo(task="sentiment", model="brainbert.base.ko.shopping", lang="ko")
+
+# Load named entity recognition model
+with st.spinner('Loading sentiment_analysis model...'):
+    ner_model = Pororo(task="ner", lang="ko")
+
