6fdd973941f81b439b031766229566fa3be51ddd
chee_mac <chee_mac@chee-macui-Macmini.local>
2021-06-02 14:52:02 +0900

    fix indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,62 +4,66 @@ from spacy import displacy
 import streamlit as st
 import random
 
-print (Pororo.available_tasks())
+print(Pororo.available_tasks())
 
 # Set page title
-st.title('Rapid pOroro Demo Inferencer')
+st.title("Rapid pOroro Demo Inferencer")
 
 # Load similarity model
 @st.cache(allow_output_mutation=True)
 def load_similarity_model():
-    with st.spinner('Loading similarity model...'):
+    with st.spinner("Loading similarity model..."):
         similarity_model = Pororo(task="similarity", lang="ko")
 
         return similarity_model
 
+
 # Load sentiment_analysis model
 @st.cache(allow_output_mutation=True)
 def load_sentiment_model():
-    with st.spinner('Loading sentiment_analysis model...'):
-        sentiment_model = Pororo(task="sentiment", model="brainbert.base.ko.shopping", lang="ko")
+    with st.spinner("Loading sentiment_analysis model..."):
+        sentiment_model = Pororo(
+            task="sentiment", model="brainbert.base.ko.shopping", lang="ko"
+        )
 
         return sentiment_model
+
 
 # Load named entity recognition model
 @st.cache(allow_output_mutation=True)
 def load_ner_model():
-    with st.spinner('Loading sentiment_analysis model...'):
+    with st.spinner("Loading sentiment_analysis model..."):
         ner_model = Pororo(task="ner", lang="ko")
-        
+
         return ner_model
+
 
 # Load Paraphrase Identification model
 @st.cache(allow_output_mutation=True)
 def load_paraphrase_identification_model():
-    with st.spinner('Loading paraphrase_identification model...'):
+    with st.spinner("Loading paraphrase_identification model..."):
         paws_model = Pororo(task="para", lang="ko")
-        
+
         return paws_model
+
 
 # Load tts model
 @st.cache(allow_output_mutation=True)
 def load_tts_model():
-    with st.spinner('Loading tts model...'):
+    with st.spinner("Loading tts model..."):
         tts_model = Pororo(task="tts", lang="multi")
-        
+
         return tts_model
-    
+
+
 # load mt model
 def load_mt_model():
-  with st.spinner('Loading tts model...'):
-   mt = Pororo(task="translation", lang="multi")
-   
-   return mt
-    
-    
-    
-    
-    
+    with st.spinner("Loading tts model..."):
+        mt = Pororo(task="translation", lang="multi")
+
+        return mt
+
+
 def hf_ents_to_displacy_format(ents, ignore_entities=[]):
     s_ents = {}
     s_ents["text"] = " ".join([e[0] for e in ents])
@@ -84,46 +88,52 @@ def hf_ents_to_displacy_format(ents, ignore_entities=[]):
 
     return s_ents
 
+
 def add_colormap(labels):
     color_map = {}
     for label in labels:
-        #if label not in color_map:
-        rand_color = "#"+"%06x" % random.randint(0, 0xFFFFFF)
-        color_map[label]=rand_color
+        # if label not in color_map:
+        rand_color = "#" + "%06x" % random.randint(0, 0xFFFFFF)
+        color_map[label] = rand_color
 
     return color_map
+
 
 # from https://github.com/explosion/spacy-streamlit/util.py#L26
 WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem; margin-bottom: 2.5rem">{}</div>"""
 
-if __name__ == '__main__':
-    st.subheader('Semantic Textual Similarity')
+if __name__ == "__main__":
+    st.subheader("Semantic Textual Similarity")
     similarity_model = load_similarity_model()
-    sim_query1_input = st.text_input('query1:')
-    sim_query2_input = st.text_input('query2:')
-    if sim_query1_input != '' and sim_query2_input != '':
-        with st.spinner('Predicting...'):
-            st.write(f'Similarity: {similarity_model(sim_query1_input, sim_query2_input)}')
+    sim_query1_input = st.text_input("query1:")
+    sim_query2_input = st.text_input("query2:")
+    if sim_query1_input != "" and sim_query2_input != "":
+        with st.spinner("Predicting..."):
+            st.write(
+                f"Similarity: {similarity_model(sim_query1_input, sim_query2_input)}"
+            )
 
-    st.subheader('Sentiment Analysis')
+    st.subheader("Sentiment Analysis")
     sentiment_model = load_sentiment_model()
-    sentiment_query_input = st.text_input('sentiment query:')
-    if sentiment_query_input != '':
-        with st.spinner('Predicting...'):
-            st.write('Result:')
+    sentiment_query_input = st.text_input("sentiment query:")
+    if sentiment_query_input != "":
+        with st.spinner("Predicting..."):
+            st.write("Result:")
             st.json(sentiment_model(sentiment_query_input, show_probs=True))
 
-    st.subheader('Named Entity Recognition')
+    st.subheader("Named Entity Recognition")
     ner_model = load_ner_model()
-    ner_query_input = st.text_input('ner query:')
-    if ner_query_input != '':
-        with st.spinner('Predicting...'):
-            st.write('Result:')
-            bert_doc = hf_ents_to_displacy_format(ner_model(ner_query_input), ignore_entities=["O"])
+    ner_query_input = st.text_input("ner query:")
+    if ner_query_input != "":
+        with st.spinner("Predicting..."):
+            st.write("Result:")
+            bert_doc = hf_ents_to_displacy_format(
+                ner_model(ner_query_input), ignore_entities=["O"]
+            )
             labels = list(set([a["label"] for a in bert_doc["ents"]]))
             color_map = add_colormap(labels)
-            html = displacy.render(bert_doc, manual=True, style="ent", options={"colors": color_map})
+            html = displacy.render(
+                bert_doc, manual=True, style="ent", options={"colors": color_map}
+            )
             html = html.replace("\n", " ")
             st.write(WRAPPER.format(html), unsafe_allow_html=True)
-
-
