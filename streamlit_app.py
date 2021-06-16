98d9688d6ce1d8b35ca19f29e997cd4ee8b223a0
Ubuntu <ubuntu@ip-172-31-0-244.ap-northeast-2.compute.internal>
2021-06-16 10:50:48 +0000

    modify image upload logic 3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 from pororo import Pororo
 from spacy import displacy
+from PIL import Image
 
 import streamlit as st
+import numpy as np
 import random
 
 # Set page title
@@ -269,10 +271,9 @@ if __name__ == "__main__":
         ocr_model = load_ocr_model()
         uploaded_file = st.file_uploader("Upload Image file", type=['png','jpg','jpeg'])
 
-        print (uploaded_file)
-
         if uploaded_file is not None:
-            st.json(ocr_model(uploaded_file, detail=True))
+            image = np.array(Image.open(uploaded_file))
+            st.json(ocr_model(image, detail=True))
         
         st.markdown("""---""")
 
